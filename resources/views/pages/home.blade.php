7e9681c74a21562a801ec16195d029e51022573c
aweh23 <aweh.felix@moyoweb.nl>
2015-08-30 23:13:15 +0200

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -22,17 +22,17 @@
     <div class="container">
         <div class="flex-boxes">
           <a href="{{url('services')}}" class="flex-box">
-            <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_2_dark.png" alt="">
+            <img src="/image/homepage/services.png" alt="services">
             <h1 class="flex-title">Services</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae quis ipsum, officia, aperiam tenetur dolor molestiae voluptate perferendis dolorem vel ex, unde fugit blanditiis sapiente.</p>
           </a>
           <a href="{{url('portfolios')}}" class="flex-box">
-            <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_2_dark.png" alt="">
+            <img src="/image/homepage/portfolio.png" alt="portfolio">
             <h1 class="flex-title">Portfolio</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo obcaecati in provident illo.</p>
           </a>
           <a href="{{url('about')}}" class="flex-box">
-            <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_3_dark.png" alt="">
+            <img src="/image/homepage/about.png" alt="about us">
             <h1 class="flex-title">About Us</h1>
             <p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
           </a>
